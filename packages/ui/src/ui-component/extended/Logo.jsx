944f1097ab640e65fc7fd8c45b25cd72aab4fa51
Marco Bellante <103267756+marco-ncode@users.noreply.github.com>
2025-07-19 00:12:15 +0200

    Update Logo.jsx

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_white.svg'
-import logoDark from '@/assets/images/flowise_dark.svg'
+import logo from '@/assets/images/GAIA_white.svg'
+import logoDark from '@/assets/images/GAIA_dark.svg'
 
 import { useSelector } from 'react-redux'
 
